3d38196d39ae0c0979b589a8a2bfbc9a766d58a2
viniciuspadovam <padovam3373@gmail.com>
2022-01-10 01:43:33 -0300

    Add Basic Text

--- main.py
+++ main.py
@@ -5,10 +5,11 @@ pygame.init()
 screen = pygame.display.set_mode((800, 400))
 pygame.display.set_caption('Denno Runner')
 clock = pygame.time.Clock()
+test_font = pygame.font.Font('assets/fonts/Pixeltype.ttf', 50)
 
 sky_surface = pygame.image.load('assets/graphics/sky.png')
 ground_surface = pygame.image.load('assets/graphics/ground.png')
-
+text_surface = test_font.render('My game', False, 'Black')
 
 while True:
     # Draw and update all elementes
@@ -19,6 +20,7 @@ while True:
 
     screen.blit(sky_surface, (0, 0))
     screen.blit(ground_surface, (0, 300))
+    screen.blit(text_surface, (300, 50))
     
     pygame.display.update()
     clock.tick(60) 
