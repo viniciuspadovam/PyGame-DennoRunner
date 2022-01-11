6c966b4991fb367b24f1f062938805c87e94d86e
viniciuspadovam <padovam3373@gmail.com>
2022-01-11 02:32:13 -0300

    Add animations

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ def obstacle_movement(obstacle_list):
             obstacle_rect.x -= 5
 
             if obstacle_rect.bottom == 300: screen.blit(chicken_surface, obstacle_rect)
-            else: screen.blit(enemy_surface, obstacle_rect)
+            else: screen.blit(bullet_surface, obstacle_rect)
         
         obstacle_list = [obstacle for obstacle in obstacle_list if obstacle.x > -100]
         return obstacle_list
@@ -27,6 +27,16 @@ def collisions(player, obstacles):
         for obstacle_rect in obstacles:
             if player.colliderect(obstacle_rect): return False
     return True
+
+def player_animation():
+    global player_surface, player_index
+
+    if player_rect.bottom < 300:
+        player_surface = player_jump
+    else:
+        player_index += 0.1
+        if player_index >= len(player_walk): player_index = 0
+        player_surface = player_walk[int(player_index)]
 
 pygame.init()
 screen = pygame.display.set_mode((800, 400))
@@ -42,13 +52,32 @@ ground_surface = pygame.image.load('assets/graphics/ground.png').convert()
 # score_surface = test_font.render('My game', False, (64, 64, 64))
 # score_rect = score_surface.get_rect(midtop = (int(screen.get_width() / 2), 50))
 
-# --- Enemy ---
-enemy_surface = pygame.image.load('assets/graphics/enemy/enemy.png').convert_alpha()
-chicken_surface = pygame.image.load('assets/graphics/enemy2/chicken.png').convert_alpha()
+# --- Chicken ---
+chicken_frame_1 = pygame.image.load('assets/graphics/Chicken/chicken1.png').convert_alpha()
+chicken_frame_2 = pygame.image.load('assets/graphics/Chicken/chicken2.png').convert_alpha()
+chicken_frame_3 = pygame.image.load('assets/graphics/Chicken/chicken3.png').convert_alpha()
+chicken_frames = [chicken_frame_1, chicken_frame_2, chicken_frame_3]
+chicken_frame_index = 0
+chicken_surface = chicken_frames[chicken_frame_index]
+
+# --- Bullet ---
+bullet_frame_1 = pygame.image.load('assets/graphics/Bullet/bullet_1.png').convert_alpha()
+bullet_frame_2 = pygame.image.load('assets/graphics/Bullet/bullet_2.png').convert_alpha()
+bullet_frame_3 = pygame.image.load('assets/graphics/Bullet/bullet_3.png').convert_alpha()
+bullet_frames = [bullet_frame_1, bullet_frame_2, bullet_frame_3]
+bullet_frame_index = 0
+bullet_surface = bullet_frames[bullet_frame_index]
 
 obstacle_rect_list = []
 
-player_surface = pygame.image.load('assets/graphics/player/player_stand.png').convert_alpha()
+# --- Player ---
+player_walk_1 = pygame.image.load('assets/graphics/player/player_walk_1.png').convert_alpha()
+player_walk_2 = pygame.image.load('assets/graphics/player/player_walk_2.png').convert_alpha()
+player_walk = [player_walk_1, player_walk_2]
+player_index = 0
+player_jump = pygame.image.load('assets/graphics/player/player_jump.png').convert_alpha()
+
+player_surface = player_walk[player_index]
 player_rect = player_surface.get_rect(midbottom = (80, 300))
 player_gravity = 0
 
@@ -67,6 +96,13 @@ instruction_txt_rect = instruction_txt.get_rect(center = (400, 350))
 # --- Timer ---
 obstacle_timer = pygame.USEREVENT + 1
 pygame.time.set_timer(obstacle_timer, 1500)
+
+chicken_animation_timer = pygame.USEREVENT + 2
+pygame.time.set_timer(chicken_animation_timer, 180)
+
+bullet_animation_timer = pygame.USEREVENT + 3
+pygame.time.set_timer(bullet_animation_timer, 380)
+
 
 # Draw and update all elementes
 while True:
@@ -87,7 +123,19 @@ while True:
                 if randint(0, 2):
                     obstacle_rect_list.append(chicken_surface.get_rect(bottomleft = (randint(900, 1100), 300)))
                 else:
-                    obstacle_rect_list.append(enemy_surface.get_rect(bottomleft = (randint(900, 1100), 150)))
+                    obstacle_rect_list.append(bullet_surface.get_rect(bottomleft = (randint(900, 1100), 150)))
+
+            if event.type == chicken_animation_timer:
+                if chicken_frame_index == 0: chicken_frame_index = 1
+                elif chicken_frame_index == 1: chicken_frame_index = 2
+                else: chicken_frame_index = 0
+                chicken_surface = chicken_frames[chicken_frame_index]
+
+            if event.type == bullet_animation_timer:
+                if bullet_frame_index == 0: bullet_frame_index = 1
+                elif bullet_frame_index == 1: bullet_frame_index = 2
+                else: bullet_frame_index = 0
+                bullet_surface = bullet_frames[bullet_frame_index]
 
         else:
             if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
@@ -104,12 +152,13 @@ while True:
         
         # enemy_rect.x -= 5
         # if enemy_rect.left < -100: enemy_rect.left = 805
-        # screen.blit(enemy_surface, enemy_rect)
+        # screen.blit(bullet_surface, enemy_rect)
 
         # --- Player ---
         player_gravity += 1
         player_rect.y += player_gravity
         if player_rect.bottom >= 300: player_rect.bottom = 300
+        player_animation()
         screen.blit(player_surface, player_rect)
 
         # --- Enemy Movement
@@ -123,6 +172,8 @@ while True:
         screen.blit(name_txt, name_txt_rect)
         screen.blit(player_stand, player_stand_rect)
         obstacle_rect_list.clear()
+        player_rect.midbottom = (80, 300)
+        player_gravity = 0
 
         score_txt = test_font.render(f'Your Score: {score}', False, (111, 196, 169))
         score_txt_rect = score_txt.get_rect(center = (400, 350))
