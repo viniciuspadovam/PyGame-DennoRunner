d9338974615e639239a283b5cdf7d9fc35fd08d3
viniciuspadovam <padovam3373@gmail.com>
2022-01-11 01:44:51 -0300

    Better enemy spawn and collision fix

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame
+from random import randint
 from sys import exit
 from pygame.constants import K_SPACE
 
@@ -9,12 +10,30 @@ def display_score():
     screen.blit(score_surface, score_rect)
     return current_time
 
+def obstacle_movement(obstacle_list):
+    if obstacle_list:
+        for obstacle_rect in obstacle_list:
+            obstacle_rect.x -= 5
+
+            if obstacle_rect.bottom == 300: screen.blit(chicken_surface, obstacle_rect)
+            else: screen.blit(enemy_surface, obstacle_rect)
+        
+        obstacle_list = [obstacle for obstacle in obstacle_list if obstacle.x > -100]
+        return obstacle_list
+    else: return []
+
+def collisions(player, obstacles):
+    if obstacles:
+        for obstacle_rect in obstacles:
+            if player.colliderect(obstacle_rect): return False
+    return True
+
 pygame.init()
 screen = pygame.display.set_mode((800, 400))
 pygame.display.set_caption('Pixel Runner')
 clock = pygame.time.Clock()
 test_font = pygame.font.Font('assets/fonts/Pixeltype.ttf', 50)
-game_active = True
+game_active = False
 start_time = 0
 score = 0
 
@@ -23,8 +42,11 @@ ground_surface = pygame.image.load('assets/graphics/ground.png').convert()
 # score_surface = test_font.render('My game', False, (64, 64, 64))
 # score_rect = score_surface.get_rect(midtop = (int(screen.get_width() / 2), 50))
 
-enemy_surface = pygame.image.load('assets/graphics/enemy.png').convert_alpha()
-enemy_rect = enemy_surface.get_rect(topleft = (600, 250))
+# --- Enemy ---
+enemy_surface = pygame.image.load('assets/graphics/enemy/enemy.png').convert_alpha()
+chicken_surface = pygame.image.load('assets/graphics/enemy2/chicken.png').convert_alpha()
+
+obstacle_rect_list = []
 
 player_surface = pygame.image.load('assets/graphics/player/player_stand.png').convert_alpha()
 player_rect = player_surface.get_rect(midbottom = (80, 300))
@@ -42,6 +64,10 @@ name_txt_rect = name_txt.get_rect(center = (400, 50))
 instruction_txt = test_font.render('Press Space to Start', False, (111, 196, 169))
 instruction_txt_rect = instruction_txt.get_rect(center = (400, 350))
 
+# --- Timer ---
+obstacle_timer = pygame.USEREVENT + 1
+pygame.time.set_timer(obstacle_timer, 1500)
+
 # Draw and update all elementes
 while True:
     # --- EVENT LOOP ---
@@ -56,10 +82,16 @@ while True:
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_SPACE and player_rect.bottom == 300:
                     player_gravity = -20
+            
+            if event.type == obstacle_timer:
+                if randint(0, 2):
+                    obstacle_rect_list.append(chicken_surface.get_rect(bottomleft = (randint(900, 1100), 300)))
+                else:
+                    obstacle_rect_list.append(enemy_surface.get_rect(bottomleft = (randint(900, 1100), 150)))
+
         else:
             if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
                 game_active = True
-                enemy_rect.left = 805
                 start_time = int(pygame.time.get_ticks() / 1000)
 
     if game_active:
@@ -70,23 +102,27 @@ while True:
         # screen.blit(score_surface, score_rect)
         score = display_score()
         
-        enemy_rect.x -= 5
-        if enemy_rect.left < -100: enemy_rect.left = 805
-        screen.blit(enemy_surface, enemy_rect)
+        # enemy_rect.x -= 5
+        # if enemy_rect.left < -100: enemy_rect.left = 805
+        # screen.blit(enemy_surface, enemy_rect)
 
         # --- Player ---
         player_gravity += 1
         player_rect.y += player_gravity
         if player_rect.bottom >= 300: player_rect.bottom = 300
         screen.blit(player_surface, player_rect)
+
+        # --- Enemy Movement
+        obstacle_rect_list = obstacle_movement(obstacle_rect_list)
+
+        # --- Collisions ---
+        game_active = collisions(player_rect, obstacle_rect_list)
         
-        # --- Collision ---
-        if enemy_rect.colliderect(player_rect):
-            game_active = False
     else:
         screen.fill((94, 129, 162))
         screen.blit(name_txt, name_txt_rect)
         screen.blit(player_stand, player_stand_rect)
+        obstacle_rect_list.clear()
 
         score_txt = test_font.render(f'Your Score: {score}', False, (111, 196, 169))
         score_txt_rect = score_txt.get_rect(center = (400, 350))
